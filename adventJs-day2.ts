1f467eb63f0885675c8f26b8b92401540cb71f5f
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-12-03 19:58:51 +0100

    solution for day 2

--- adventJs-day2.ts
+++ adventJs-day2.ts
@@ -24,6 +24,16 @@ manufacture(gifts, materials) // []
 */
 
 function manufacture(gifts, materials) {
-    // Code here
-    return []
+  let soluciones : string[] = [];
+  let materialsArray = materials.split('');
+
+  for (let i = 0; i < gifts.length; i++) {
+    let regaloValido = gifts[i].split('').every(gift => materialsArray.includes(gift));
+
+    if (regaloValido) {
+      soluciones.push(gifts[i]);
+    }
   }
+
+  return soluciones;
+}
