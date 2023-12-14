c56835cf06bab722647bd0bf394fa10d3e2ebb21
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-12-14 13:38:53 +0100

    Solution for day 6

--- 06/adventJs-day6.ts
+++ 06/adventJs-day6.ts
@@ -1,4 +1,18 @@
-function maxDistance(movements) {
-  // Code here
-  return 0;
+function maxDistance(movements: string): number {
+  const steps = [...movements];
+
+  let distanciaRecorrida = 0;
+  let asteriscos = 0;
+
+  for (const step of steps) {
+    if (step === ">") {
+      distanciaRecorrida++;
+    } else if (step === "<") {
+      distanciaRecorrida--;
+    } else if (step === "*") {
+      asteriscos++;
+    }
+  }
+
+  return Math.abs(distanciaRecorrida) + asteriscos;
 }
