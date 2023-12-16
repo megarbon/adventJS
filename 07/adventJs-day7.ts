29726d29addd5156d930ba73fcaf5d1c18c26869
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-12-16 16:02:31 +0100

    Update adventJs-day7.ts

--- 07/adventJs-day7.ts
+++ 07/adventJs-day7.ts
@@ -1,4 +1,24 @@
 function drawGift(size, symbol) {
-  // Code here
-  return "#\n";
+  let result = '';
+
+  // Drawing the top of the gift
+  for (let i = size - 1; i >= 0; i--) {
+      const spaces = ' '.repeat(i);
+      const sides = '#'.repeat(size - i);
+      const top = '#'.repeat(i + 1);
+      result += `${spaces}${top}${symbol.repeat(2 * (size - i) - 1)}${sides}\n`;
+  }
+
+  // Drawing the bottom of the gift
+  for (let i = 1; i < size; i++) {
+      const spaces = ' '.repeat(i);
+      const sides = '#'.repeat(size - i);
+      const bottom = '#'.repeat(size - i);
+      result += `${spaces}${bottom}${symbol.repeat(2 * i - 1)}${sides}\n`;
+  }
+
+  // Adding a newline at the end
+  result += '\n';
+
+  return result;
 }
